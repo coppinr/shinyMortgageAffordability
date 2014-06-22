60360a82e8956489709e696f1f813714a670edd5
Rhiannon Coppin <coppinr@gmail.com>
2014-06-22 08:43:00 -0700

    first commit

---
+++ server.R
@@ -0,0 +1,34 @@
+library(UsingR)
+#source("http://faculty.ucr.edu/~tgirke/Documents/R_BioCond/My_R_Scripts/mortgage.R")
+shinyServer(
+  function(input, output) {
+    
+    loan_amount <- reactive(input$total_cost-input$down_payment)
+    down_percent <- reactive(input$down_payment/input$total_cost)
+    output$text1 <- renderText({paste("Your down payment of $", input$down_payment," represents ", round(down_percent()*100,2), "% of the purchase price, and leaves you needing a total loan of $", prettyNum(loan_amount()*1000, scientific=FALSE, big.mark=","), ".\n", sep="")})
+#    P <- reactive(loan_amount)()
+#    i <- reactive(input$interest_rate)()
+#    n <- reactive(input$mortgage_term)()
+    #interest_rate <- reactive((1+(input$interest_rate)/2)^2-1)
+    ### Formula for semi-annual interest compounding from http://www.drcalculator.com/mortgage/instructions.html
+    #Monthly Pmt = (P*(((1+i/200)^(1/6)-1))/(1-(((1+i/200)^(1/6)))^-(n*12)))
+    #monthly_payment <- reactive((input$total_cost-input$down_payment)*(((1+input$interest_rate/200)^(1/6)-1))/(1-(((1+input$interest_rate/200)^(1/6)))^-(input$mortgage_term*12)))
+    monthly_payment <- reactive((input$total_cost-input$down_payment)*1000*(((1+input$interest_rate/200)^(1/6)-1))/(1-(((1+input$interest_rate/200)^(1/6)))^-(input$mortgage_term*12)))
+    monthly_housing <- reactive(monthly_payment() + input$property_tax/12 + input$strata_fee)
+    output$text2 <- renderText({paste("You can afford a monthly payment of up to $", prettyNum(input$monthly_income-input$monthly_expenses, scientific=FALSE, big.mark=","), " and under the current terms (",input$mortgage_term," years and interest rate of",input$interest_rate,"%), your monthly housing cost (mortgage payment of $", prettyNum(round(monthly_payment()), big.mark=","), " plus property tax and strata fees) is roughly $", prettyNum(round(monthly_housing()), big.mark=","), ".", sep="")})
+    output$text3 <- renderUI({em(
+      if (down_percent() < 0.2){
+        paste("Note: As your down payment is less than 20% of the purchase price, you will have to pay an additional CMHC premium if this purchase is in Canada.")
+        })
+    })
+    output$chart1 <- renderPlot({
+      colour <- c("grey", "grey", "grey", "black")
+      if (input$monthly_income < input$monthly_expenses + monthly_housing()) {
+        colour <- c("grey", "grey", "grey", "red")
+      }
+      data <- c(input$monthly_income, input$monthly_expenses, monthly_housing(), input$monthly_income-input$monthly_expenses-monthly_housing() )  
+      bp <-  barplot(data, main="Monthly Affordability", ylab="Dollars", col = colour, names.arg=c("Income", "Expenses", "Housing cost", "Excess/Deficit"))
+      text(bp, 0, round(data, 0),cex=1,pos=3)
+    })  
+  }
+)
---
+++ ui.R
@@ -0,0 +1,46 @@
+price <<- 300
+shinyUI(pageWithSidebar(
+  
+  headerPanel("Mortgage affordability calculator"),
+
+  sidebarPanel(
+    #p("This ShinyApp will help you figure out if you can afford that condo, loft, townhouse or house you're after; but ultimately, the decision is up to what you already have saved, what the seller is willing to go with, and what terms a lender will offer you. Individual results will vary."),
+    p("This ShinyApp will help you figure out if you can afford that condo, loft, townhouse or house you're after."),
+    
+    #Mortgage and property information
+    h4("The property"),
+    
+    numericInput("total_cost", "Price (in thousands)", price, min=0),
+    #uiOutput("total_cost_widget"),
+    helpText("Accepted offer price, in thousands (so $300,000 is just '300')."),
+    numericInput("property_tax", 'Annual property tax (dollars)', 1500, min=0),
+    #uiOutput("down_payment_widget"),
+    #sliderInput('down_payment', 'Downpayment (in thousands)',value = 15, min = 0, max = price, step = 5,),
+    numericInput("strata_fee", 'Monthly strata/maintenance fees (dollars)', 250, min=0),
+    
+    #Mortgage information
+    h4("The mortgage"),
+    numericInput("interest_rate", "Interest rate (%)", 3.00, min=0),
+    helpText("Start at the current prime rate if you don't know what your bank will offer you. We are going to assume semi-anual compounding"),
+
+    sliderInput('mortgage_term', 'Term (years)',value = 25, min = 5, max = 35, step = 5),
+    
+    
+    #Your information
+    h4("Your finances"),
+    numericInput("down_payment", 'Downpayment (in thousands)', price/20, min=0),
+    helpText("Cash you have saved, in thousands, minus a few thousand for closing costs"),
+    
+    numericInput("monthly_income", 'Your monthly income (dollars)', 3200, min=0),
+    numericInput("monthly_expenses", 'Your monthly expenses (dollars)', 1900, min=0),
+    helpText("Don't include rent, but include food, transportation, power, and any other existing living expenses.")
+    
+  ),
+  mainPanel(
+    textOutput("text1"),
+    textOutput("text2"),
+    htmlOutput("text3"),
+    plotOutput("chart1")
+    
+  )
+))
